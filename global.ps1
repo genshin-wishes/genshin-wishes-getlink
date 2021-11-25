1cc8f1ab62be0eb0ff47822d092d275e1cbbbb9d
Jimmy <1994206+jvergerolle@users.noreply.github.com>
2021-11-25 03:31:12 +0100

    Wait before exit

--- global.ps1
+++ global.ps1
@@ -2,11 +2,17 @@
 $path = [System.Environment]::ExpandEnvironmentVariables($logLocation);
 if (-Not [System.IO.File]::Exists($path)) {
     Write-Host "We cannot find the log file! Make sure to open the wish history ingame first!" -ForegroundColor Red
+    Write-Host 'Press any key to quit...'
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+    exit
 }
 $logs = Get-Content -Path $path
 $match = $logs -match "^OnGetWebViewPageFinish.*log$"
 if (-Not $match) {
     Write-Host "We cannot find the wish history url! Make sure to open the wish history ingame first!" -ForegroundColor Red
+    Write-Host 'Press any key to quit...'
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+    exit
 }
 [string] $wishHistoryUrl = $match -replace 'OnGetWebViewPageFinish:', ''
 Write-Host $wishHistoryUrl
