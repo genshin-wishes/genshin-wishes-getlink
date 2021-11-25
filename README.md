7e29f1f1b9a38d962df3c63c87c29a1990388625
Jimmy <1994206+jvergerolle@users.noreply.github.com>
2021-11-25 12:02:08 +0100

    Added credits and updated scripts

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # genshin-wishes-getlink
+*Based on MadeBaruna's [getlink gist](https://gist.github.com/MadeBaruna/1d75c1d37d19eca71591ec8a31178235)*
 
-## How to use it ?
+# How to use it ?
 
-### Europe / America / Asia (Outside Mainland China)
+## Europe / America / Asia (Outside Mainland China)
 1. Open Genshin Impact on your PC
 2. Then go to the wish history page and wait it to load
 3. Go back to Windows
@@ -14,7 +15,7 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercon
 6. Hit ENTER then a link will be copied to your clipboard
 7. Paste the link on genshin-wishes.com to synchronise your wishes
 
-### Mainland China
+## Mainland China
 1. Open Genshin Impact on your PC
 2. Then go to the wish history page and wait it to load
 3. Go back to Windows
--- mainland-china.ps1
+++ mainland-china.ps1
@@ -1,18 +1,14 @@
-﻿$logLocation = "%userprofile%\AppData\LocalLow\miHoYo\原神\output_log.txt";
+﻿$logLocation = "%userprofile%\AppData\LocalLow\miHoYo\$([char]0x539f)$([char]0x795e)\output_log.txt";
 $path = [System.Environment]::ExpandEnvironmentVariables($logLocation);
 if (-Not [System.IO.File]::Exists($path)) {
     Write-Host "We cannot find the log file! Make sure to open the wish history ingame first!" -ForegroundColor Red
-    Write-Host 'Press any key to quit...'
-    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
-    exit
+    return
 }
 $logs = Get-Content -Path $path
 $match = $logs -match "^OnGetWebViewPageFinish.*log$"
 if (-Not $match) {
     Write-Host "We cannot find the wish history url! Make sure to open the wish history ingame first!" -ForegroundColor Red
-    Write-Host 'Press any key to quit...'
-    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
-    exit
+    return
 }
 [string] $wishHistoryUrl = $match -replace 'OnGetWebViewPageFinish:', ''
 $friendlyUrl = $wishHistoryUrl.Split(" ")
